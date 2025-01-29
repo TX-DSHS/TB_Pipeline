6190fad68d70ab997061be81fe2631caeb18baff
meganpartridge <141433381+meganpartridge@users.noreply.github.com>
2025-01-29 13:56:19 -0600

    Delete submit_to_SRA.sh

--- submit_to_SRA.sh
+++
@@ -1,5 +0,0 @@
-aws_bucket="s3://804609861260-bioinformatics-infectious-disease"
-reads="/home/lab/tbCDC/reads"
-work_dir="/home/lab/tbCDC/varpipe_wgs/data"
-
-/home/dnalab/.aspera/connect/bin/ascp -i /home/dnalab/aspera.openssh -QT -l100m -k1 -d $reads/$1/ subasp@upload.ncbi.nlm.nih.gov:uploads/lab.microbiology_dshs.state.tx.us_rJiZeQDA/$1
